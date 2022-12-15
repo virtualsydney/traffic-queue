ed9d5211814f22e996e5a526fd889bba41bfa89f
Artyom Golovko <zordeizm@yandex.ru>
2022-12-15 13:29:55 +0300

    Clean code

--- queue.js
+++ queue.js
@@ -1,11 +1,11 @@
 'use strict'
 
-class Queue {
+module.exports.Queue = class Queue {
   constructor(concurrency) {
     this.concurrency = concurrency
     this.count = 0
     this.waiting = []
-    this.onProcess = null;
+    this.onProcess = null
     this.onSuccess = null
     this.onFailure = null
     this.onDone = null
@@ -18,10 +18,9 @@ class Queue {
 
   async add(task) {
     const hasChannel = this.count < this.concurrency
-    this.waiting.push(task);
+    if (!this.waiting.includes(task)) this.waiting.push(task)
     if (hasChannel) {
-      await this.next();
-      return;
+      await this.next()
     }
   }
 
@@ -35,62 +34,43 @@ class Queue {
         this.onSuccess(result);
         await this.onDone(filepath)
         if (this.waiting.length > 0) {
-          const nextTask = this.waiting[0];
-          await this.next(nextTask);
-          return;
+          const nextTask = this.waiting[0]
+          await this.next(nextTask)
+          return
         }
       } else {
         this.waiting.push(filepath)
       }
     } catch (err) {
-      await this.onFailure(err, filepath);
+      await this.onFailure(err, filepath)
       await this.onDone(filepath)
     } finally {
-      this.count--;
+      this.count--
     }
   }
 
   process(listener) {
-    this.onProcess = listener;
-    return this;
+    this.onProcess = listener
+    return this
   }
 
   success(listener) {
-    this.onSuccess = listener;
-    return this;
+    this.onSuccess = listener
+    return this
   }
 
   failure(listener) {
-    this.onFailure = listener;
-    return this;
+    this.onFailure = listener
+    return this
   }
 
   done(listener) {
-    this.onDone = listener;
-    return this;
+    this.onDone = listener
+    return this
   }
 
   validate(listener) {
-    this.onValidate = listener;
-    return this;
+    this.onValidate = listener
+    return this
   }
 }
-
-module.exports = { Queue }
-
-// Usage
-
-// const job = (task, next) => {
-//   console.log(`Process: ${task.name}`);
-//   setTimeout(next, task.interval, null, task);
-// };
-
-// const queue = Queue.channels(3)
-//   .process(job)
-//   .success((res) => console.log(`Success: ${res.name}`))
-//   .failure((err) => console.log(`Failure: ${err}`))
-// for (let i = 0; i < 10; i++) {
-//   queue.add({ name: `Task${i}`, interval: i * 1000 });
-// }
-
-// module.exports.Queue = Queue
